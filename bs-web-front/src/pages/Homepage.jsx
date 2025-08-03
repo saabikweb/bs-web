5dee9cef5a4614b609fe4500dceec41680bd5aec
saabikweb <mohammedshaabik@gmail.com>
2025-08-03 19:08:04 +0530

    tailwind css installed

---
+++ bs-web-front/src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Burando Studios</h1>
+        <a href="https://instagram.com/burandostudios" target="_blank">Click me </a>
+      </div>
+    </>
+  )
+}
+
+export default App
